4795fcb7f4237004d2649d6ddf447c3df290638e
mahinour30 <mahinour1233@gmail.com>
2021-01-14 19:55:46 +0200

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,51 +1,51 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import './Cards.css';
 import CardItem from './CardItem';
 
-const Cards=()=>{
-    return(
-        <div className='cards'>
-            <h1>Check Out These EPIC Destinations!</h1>
-            <div className='cards__container'>
-                <div className='cards__wrapper'>
-                    <ul className='cards__items'>
-                        <CardItem
-                        SRC='images/3.jpg'
-                        text = 'whatever'
-                        label='Adventure'
-                        path='/services'
-                        />
-                        <CardItem
-                        SRC='images/4.jpg'
-                        text = 'whatever'
-                        label='Adventure'
-                        path='/services'
-                        />
-                    </ul>
-                    <ul className='cards__items'>
-                        <CardItem
-                        SRC='images/3.jpg'
-                        text = 'whatever'
-                        label='Adventure'
-                        path='/services'
-                        />
-                        <CardItem
-                        SRC='images/4.jpg'
-                        text = 'whatever'
-                        label='Adventure'
-                        path='/services'
-                        />
-                        <CardItem
-                        SRC='images/6.jpg'
-                        text = 'whatever'
-                        label='Adventure'
-                        path='/services'
-                        />
-                    </ul>
-                </div>
-            </div>
+const Cards=()=> {
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='../images/1.jpg'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='../images/1.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='../images/1.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='../images/1.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='../images/1.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Cards;
