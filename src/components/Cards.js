e835251895fe16cbf2da7ce973cb4e4699dd5016
mahinour30 <mahinour1233@gmail.com>
2021-01-15 00:04:03 +0200

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
 
+import img6 from '../images/img6.jpg'
+import img4 from '../images/img4.jpg'
+import img8 from '../images/img8.jpg'
+import img9 from '../images/img9.jpg'
+import img10 from '../images/img10.jpg'
+
 const Cards=()=> {
   return (
     <div className='cards'>
@@ -10,13 +16,13 @@ const Cards=()=> {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='/images/1.jpg'
+              src={img8}
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
               label='Adventure'
               path='/services'
             />
             <CardItem
-              src='../images/1.jpg'
+              src={img9}
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
               path='/services'
@@ -24,19 +30,19 @@ const Cards=()=> {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='../images/1.jpg'
+              src={img10}
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='../images/1.jpg'
+              src={img4}
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='../images/1.jpg'
+              src={img6}
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
               path='/sign-up'
