e9eccea56fbcfea456505705ae3c0a0087630e9b
mahinour30 <mahinour1233@gmail.com>
2021-01-14 19:55:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
@@ -10,15 +10,15 @@ import SignUp from './components/pages/SignUp';
 function App() {
   return (
     <>
-    <Router>
-    <Navbar /> 
-    <Switch>
-      <Route path='/'exact component={Home} />
-      <Route path='/Services'exact component={Services} />
-      <Route path='/Products'exact component={Products} />
-      <Route path='/sign-up'exact component={SignUp} />
-    </Switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
     </>
   );
 }
