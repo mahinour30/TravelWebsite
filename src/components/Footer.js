f0e5fc3dd55993252a68e27a1488845c08411d45
mahinour30 <mahinour1233@gmail.com>
2021-01-15 00:04:08 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,11 +64,11 @@ const Footer=()=>{
           <div class='social-media-wrap'>
             <div class='footer-logo'>
               <Link to='/' className='social-logo'>
-                TRVL
-                <i class='fab fa-typo3' />
+                TRAVELA
+                <i style={{paddingLeft:20}} class="fas fa-suitcase-rolling"></i>
               </Link>
             </div>
-            <small class='website-rights'>TRVL © 2020</small>
+            <small class='website-rights'>TRAVELA © 2021</small>
             <div class='social-icons'>
               <Link
                 class='social-icon-link facebook'
