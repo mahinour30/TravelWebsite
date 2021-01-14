71c9f4e22501c93cc3173e7912b745765439eaeb
mahinour30 <mahinour1233@gmail.com>
2021-01-14 02:57:28 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,11 @@ const Navbar=()=>{
         setClick(!click);
     }
 
+    const closeMobileMenu=()=>{
+        setClick(false);
+    }
+
+
     return(
         <>
           <nav className='navbar'>
@@ -18,6 +23,28 @@ const Navbar=()=>{
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nave-menu'}>
+                <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
               </div>
           </nav>
         </>
