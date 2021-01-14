eb52761b49bedeb21626e00a755de0ab06f011a4
mahinour30 <mahinour1233@gmail.com>
2021-01-14 03:55:29 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 const Navbar=()=>{
     const [click, setClick]=useState(false);
+    const [button, setButton]=useState(true);
 
     const handleClick=()=>{
         setClick(!click);
@@ -12,12 +15,27 @@ const Navbar=()=>{
         setClick(false);
     }
 
+    const showButton=()=>{
+        if(window.innerWidth<=960){
+            setButton(false);  
+        } else {
+            setButton(true)
+        }
+    };
+
+    useEffect(()=>{
+        showButton();
+    },[])
+
+    window.addEventListener('resize', showButton)
+
 
     return(
         <>
           <nav className='navbar'>
               <div className='navbar-container'>
-                <Link to='/' className='navbar-logo'>
+                <Link to='/' className='navbar-logo' 
+                onClick={closeMobileMenu}>
                     TRVL <i className='fab fa-typo3' />
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
@@ -45,6 +63,7 @@ const Navbar=()=>{
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
               </div>
           </nav>
         </>
